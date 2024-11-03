c9ed8065593e73aeff4b02bcbc521b0844a5e97f
Lyuboslav Karev <lyubo.karev@gmail.com>
2024-11-03 23:43:31 +0200

    Fix typo in CLI

--- src/cli.rs
+++ src/cli.rs
@@ -14,7 +14,7 @@ pub fn build_cli() -> Command {
         )
         .arg(arg!(-f --fields <LIST>))
         .arg(arg!(--complement).action(ArgAction::SetTrue))
-        .arg(arg!(-s - -only_delimited).action(ArgAction::SetTrue))
+        .arg(arg!(-s --only_delimited).action(ArgAction::SetTrue))
         .arg(arg!(--output_delimiter <DELIM>).default_value("\n"))
         .arg(arg!(-z - -zero_terminated).action(ArgAction::SetTrue))
         .arg(arg!([FILE]).default_value("-"))
