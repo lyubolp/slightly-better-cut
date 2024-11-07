cd998a4b9ea59282cf5ed3c9f0867e0782934eec
Lyuboslav Karev <lyubo.karev@gmail.com>
2024-11-07 23:07:29 +0200

    Moved some code around in main

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod range_parser;
 
 use std::fs::File;
 use std::io::{self, Read};
+use std::process::exit;
 
 use cut::{cut_line_with_bytes, cut_line_with_characters, cut_line_with_delimiter};
 use range_parser::parse_range;
@@ -14,20 +15,12 @@ fn main() {
     let default_file = String::from("-");
     let file_path = args.get_one::<String>("FILE").unwrap_or(&default_file);
 
+    let content_result = read_content(file_path, &default_file);
 
-    let mut content: String = String::new();
-
-    if *file_path == default_file {
-        // TODO - stdin implementation
-        let mut buffer = String::new();
-        io::stdin().read_to_string(&mut content);
+    if content_result.is_err() {
+        exit(1);
     }
-    else {
-        // Read from the file
-        let file = File::open(file_path).unwrap();
-        let mut reader = io::BufReader::new(file);
-        reader.read_to_string(&mut content);
-    }
+    let content = content_result.unwrap();
 
 
     let is_using_nul_as_line_delimiter = args.get_flag("zero_terminated");
@@ -60,25 +53,49 @@ fn main() {
     let is_showing_only_delimited_lines = args.get_flag("only_delimited");
 
     let output_delimiter = match args.get_one::<String>("output_delimiter") {
-        Some(delimiter) => delimiter,
-        None => &delimiter
+        Some(delimiter) => delimiter.clone(),
+        None => delimiter.clone()
     };
 
 
+    cut_lines(lines, cut_information, is_showing_only_delimited_lines, &delimiter, &output_delimiter, is_showing_complement);
+}
+
+fn cut_lines(lines: Vec<String>, cut_information: (CutType, &String), is_showing_only_delimited_lines: bool, delimiter: &String, output_delimiter: &String, is_showing_complement: bool) {
     for line in lines {
         let (cut_type, fields) = cut_information;
 
-        if cut_type == CutType::FIELDS && is_showing_only_delimited_lines && !line.contains(&delimiter) {
+        if cut_type == CutType::FIELDS && is_showing_only_delimited_lines && !line.contains(delimiter) {
             continue;
         }
-        let n = line.split(&delimiter).count();
+        let n = line.split(delimiter).count();
         let ranges = parse_range(fields, n);
 
         let output = match ranges {
-            Ok(ranges) => cut_line(cut_type, ranges, line, &delimiter, output_delimiter, is_showing_complement),
+            Ok(ranges) => cut_line(cut_type, ranges, line, delimiter, output_delimiter, is_showing_complement),
             Err(error) => error,
         };
         println!("{}", output);
+    }
+}
+
+fn read_content(file_path: &String, default_file: &String) -> Result<String, String> {
+    let mut buffer = String::new();
+    if *file_path == *default_file {
+        let mut buffer = String::new();
+        match io::stdin().read_to_string(&mut buffer) {
+            Ok(_) => Ok(buffer),
+            Err(_) => Err(String::from("Can't read input file"))
+        }
+    }
+    else {
+        // Read from the file
+        let file = File::open(file_path).unwrap();
+        let mut reader = io::BufReader::new(file);
+        match reader.read_to_string(&mut buffer) {
+            Ok(_) => Ok(buffer),
+            Err(_) => Err(String::from("Error when reading from stdin"))
+        }
     }
 }
 
