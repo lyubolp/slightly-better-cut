0d9301c210af2a6ba35b5cbe64775c9147ddf505
lyubolp <lyubo.karev@gmail.com>
2024-11-14 22:19:34 +0200

    Formatting & disable some tests

--- tests/coreutils_cut_compatibility.rs
+++ tests/coreutils_cut_compatibility.rs
@@ -1,15 +1,15 @@
 use std::process::Command;
 
-// -b 
+// -b
 // -c
 // -d & -f
-    // For each cutter, try
-        // N
-        // N-
-        // N-M
-        // -M
-        // 1 & 2
-        // 2 & 3 & 4
+// For each cutter, try
+// N
+// N-
+// N-M
+// -M
+// 1 & 2
+// 2 & 3 & 4
 
 // --complement
 // -s
@@ -41,7 +41,7 @@ static SAMPLE_FILE: &str = "sample.csv";
 static SAMPLE_FILE_NUL_TERMINATED: &str = "sample_zero_terminated.txt";
 
 #[test]
-fn test_01_1_bytes_single_range(){
+fn test_01_1_bytes_single_range() {
     // Arrange
     let cut_arguments = vec!["-b", SINGLE_RANGE_CUT, SAMPLE_FILE];
     let sbcut_arguments = vec!["-b", SINGLE_RANGE_SBCUT, SAMPLE_FILE];
@@ -55,7 +55,7 @@ fn test_01_1_bytes_single_range(){
 }
 
 #[test]
-fn test_01_2_bytes_start_range(){
+fn test_01_2_bytes_start_range() {
     // Arrange
     let cut_arguments = vec!["-b", START_RANGE_CUT, SAMPLE_FILE];
     let sbcut_arguments = vec!["-b", START_RANGE_SBCUT, SAMPLE_FILE];
@@ -69,7 +69,7 @@ fn test_01_2_bytes_start_range(){
 }
 
 #[test]
-fn test_01_3_bytes_start_end_range(){
+fn test_01_3_bytes_start_end_range() {
     // Arrange
     let cut_arguments = vec!["-b", START_END_RANGE_CUT, SAMPLE_FILE];
     let sbcut_arguments = vec!["-b", START_END_RANGE_SBCUT, SAMPLE_FILE];
@@ -83,7 +83,7 @@ fn test_01_3_bytes_start_end_range(){
 }
 
 #[test]
-fn test_01_4_bytes_end_range(){
+fn test_01_4_bytes_end_range() {
     // Arrange
     let cut_arguments = vec!["-b", END_RANGE_CUT, SAMPLE_FILE];
     let sbcut_arguments = vec!["-b", END_RANGE_SBCUT, SAMPLE_FILE];
@@ -97,7 +97,7 @@ fn test_01_4_bytes_end_range(){
 }
 
 #[test]
-fn test_01_5_bytes_single_start_range(){
+fn test_01_5_bytes_single_start_range() {
     // Arrange
     let cut_arguments = vec!["-b", SINGLE_START_RANGE_CUT, SAMPLE_FILE];
     let sbcut_arguments = vec!["-b", SINGLE_START_RANGE_SBCUT, SAMPLE_FILE];
@@ -111,7 +111,7 @@ fn test_01_5_bytes_single_start_range(){
 }
 
 #[test]
-fn test_01_6_bytes_single_single_start_range(){
+fn test_01_6_bytes_single_single_start_range() {
     // Arrange
     let cut_arguments = vec!["-b", SINGLE_SINGLE_START_RANGE_CUT, SAMPLE_FILE];
     let sbcut_arguments = vec!["-b", SINGLE_SINGLE_START_RANGE_SBCUT, SAMPLE_FILE];
@@ -125,7 +125,7 @@ fn test_01_6_bytes_single_single_start_range(){
 }
 
 #[test]
-fn test_02_1_characters_single_range(){
+fn test_02_1_characters_single_range() {
     // Arrange
     let cut_arguments = vec!["-c", SINGLE_RANGE_CUT, SAMPLE_FILE];
     let sbcut_arguments = vec!["-c", SINGLE_RANGE_SBCUT, SAMPLE_FILE];
@@ -139,7 +139,7 @@ fn test_02_1_characters_single_range(){
 }
 
 #[test]
-fn test_02_2_characters_start_range(){
+fn test_02_2_characters_start_range() {
     // Arrange
     let cut_arguments = vec!["-c", START_RANGE_CUT, SAMPLE_FILE];
     let sbcut_arguments = vec!["-c", START_RANGE_SBCUT, SAMPLE_FILE];
@@ -153,7 +153,7 @@ fn test_02_2_characters_start_range(){
 }
 
 #[test]
-fn test_02_3_characters_start_end_range(){
+fn test_02_3_characters_start_end_range() {
     // Arrange
     let cut_arguments = vec!["-c", START_END_RANGE_CUT, SAMPLE_FILE];
     let sbcut_arguments = vec!["-c", START_END_RANGE_SBCUT, SAMPLE_FILE];
@@ -167,7 +167,7 @@ fn test_02_3_characters_start_end_range(){
 }
 
 #[test]
-fn test_02_4_characters_end_range(){
+fn test_02_4_characters_end_range() {
     // Arrange
     let cut_arguments = vec!["-c", END_RANGE_CUT, SAMPLE_FILE];
     let sbcut_arguments = vec!["-c", END_RANGE_SBCUT, SAMPLE_FILE];
@@ -181,7 +181,7 @@ fn test_02_4_characters_end_range(){
 }
 
 #[test]
-fn test_02_5_characters_single_start_range(){
+fn test_02_5_characters_single_start_range() {
     // Arrange
     let cut_arguments = vec!["-c", SINGLE_START_RANGE_CUT, SAMPLE_FILE];
     let sbcut_arguments = vec!["-c", SINGLE_START_RANGE_SBCUT, SAMPLE_FILE];
@@ -195,7 +195,7 @@ fn test_02_5_characters_single_start_range(){
 }
 
 #[test]
-fn test_02_6_characters_single_single_start_range(){
+fn test_02_6_characters_single_single_start_range() {
     // Arrange
     let cut_arguments = vec!["-c", SINGLE_SINGLE_START_RANGE_CUT, SAMPLE_FILE];
     let sbcut_arguments = vec!["-c", SINGLE_SINGLE_START_RANGE_SBCUT, SAMPLE_FILE];
@@ -209,10 +209,10 @@ fn test_02_6_characters_single_single_start_range(){
 }
 
 #[test]
-fn test_03_1_fields_single_range(){
+fn test_03_1_fields_single_range() {
     // Arrange
-    let cut_arguments = vec!["-d", ",","-f", SINGLE_RANGE_CUT, SAMPLE_FILE];
-    let sbcut_arguments = vec!["-d", ",","-f", SINGLE_RANGE_SBCUT, SAMPLE_FILE];
+    let cut_arguments = vec!["-d", ",", "-f", SINGLE_RANGE_CUT, SAMPLE_FILE];
+    let sbcut_arguments = vec!["-d", ",", "-f", SINGLE_RANGE_SBCUT, SAMPLE_FILE];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -223,10 +223,10 @@ fn test_03_1_fields_single_range(){
 }
 
 #[test]
-fn test_03_2_fields_start_range(){
+fn test_03_2_fields_start_range() {
     // Arrange
-    let cut_arguments = vec!["-d", ",","-f", START_RANGE_CUT, SAMPLE_FILE];
-    let sbcut_arguments = vec!["-d", ",","-f", START_RANGE_SBCUT, SAMPLE_FILE];
+    let cut_arguments = vec!["-d", ",", "-f", START_RANGE_CUT, SAMPLE_FILE];
+    let sbcut_arguments = vec!["-d", ",", "-f", START_RANGE_SBCUT, SAMPLE_FILE];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -237,10 +237,10 @@ fn test_03_2_fields_start_range(){
 }
 
 #[test]
-fn test_03_3_fields_start_end_range(){
+fn test_03_3_fields_start_end_range() {
     // Arrange
-    let cut_arguments = vec!["-d", ",","-f", START_END_RANGE_CUT, SAMPLE_FILE];
-    let sbcut_arguments = vec!["-d", ",","-f", START_END_RANGE_SBCUT, SAMPLE_FILE];
+    let cut_arguments = vec!["-d", ",", "-f", START_END_RANGE_CUT, SAMPLE_FILE];
+    let sbcut_arguments = vec!["-d", ",", "-f", START_END_RANGE_SBCUT, SAMPLE_FILE];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -251,10 +251,10 @@ fn test_03_3_fields_start_end_range(){
 }
 
 #[test]
-fn test_03_4_fields_end_range(){
+fn test_03_4_fields_end_range() {
     // Arrange
-    let cut_arguments = vec!["-d", ",","-f", END_RANGE_CUT, SAMPLE_FILE];
-    let sbcut_arguments = vec!["-d", ",","-f", END_RANGE_SBCUT, SAMPLE_FILE];
+    let cut_arguments = vec!["-d", ",", "-f", END_RANGE_CUT, SAMPLE_FILE];
+    let sbcut_arguments = vec!["-d", ",", "-f", END_RANGE_SBCUT, SAMPLE_FILE];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -265,10 +265,10 @@ fn test_03_4_fields_end_range(){
 }
 
 #[test]
-fn test_03_5_fields_single_start_range(){
+fn test_03_5_fields_single_start_range() {
     // Arrange
-    let cut_arguments = vec!["-d", ",","-f", SINGLE_START_RANGE_CUT, SAMPLE_FILE];
-    let sbcut_arguments = vec!["-d", ",","-f", SINGLE_START_RANGE_SBCUT, SAMPLE_FILE];
+    let cut_arguments = vec!["-d", ",", "-f", SINGLE_START_RANGE_CUT, SAMPLE_FILE];
+    let sbcut_arguments = vec!["-d", ",", "-f", SINGLE_START_RANGE_SBCUT, SAMPLE_FILE];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -279,10 +279,16 @@ fn test_03_5_fields_single_start_range(){
 }
 
 #[test]
-fn test_03_6_fields_single_single_start_range(){
+fn test_03_6_fields_single_single_start_range() {
     // Arrange
     let cut_arguments = vec!["-d", ",", "-f", SINGLE_SINGLE_START_RANGE_CUT, SAMPLE_FILE];
-    let sbcut_arguments = vec!["-d", ",","-f", SINGLE_SINGLE_START_RANGE_SBCUT, SAMPLE_FILE];
+    let sbcut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        SINGLE_SINGLE_START_RANGE_SBCUT,
+        SAMPLE_FILE,
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -291,13 +297,26 @@ fn test_03_6_fields_single_single_start_range(){
     // Assert
     assert_eq!(cut_output, sbcut_output)
 }
-
 
 #[test]
 fn test_04_1_complement_single_range() {
     // Arrange
-    let cut_arguments = vec!["-d", ",", "-f", SINGLE_RANGE_CUT, SAMPLE_FILE, "--complement"];
-    let sbcut_arguments = vec!["-d", ",","-f", SINGLE_RANGE_SBCUT, SAMPLE_FILE, "--complement"];
+    let cut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        SINGLE_RANGE_CUT,
+        SAMPLE_FILE,
+        "--complement",
+    ];
+    let sbcut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        SINGLE_RANGE_SBCUT,
+        SAMPLE_FILE,
+        "--complement",
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -310,8 +329,22 @@ fn test_04_1_complement_single_range() {
 #[test]
 fn test_04_2_complement_start_range() {
     // Arrange
-    let cut_arguments = vec!["-d", ",", "-f", START_RANGE_CUT, SAMPLE_FILE, "--complement"];
-    let sbcut_arguments = vec!["-d", ",","-f", START_RANGE_SBCUT, SAMPLE_FILE, "--complement"];
+    let cut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        START_RANGE_CUT,
+        SAMPLE_FILE,
+        "--complement",
+    ];
+    let sbcut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        START_RANGE_SBCUT,
+        SAMPLE_FILE,
+        "--complement",
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -324,8 +357,22 @@ fn test_04_2_complement_start_range() {
 #[test]
 fn test_04_3_complement_start_end_range() {
     // Arrange
-    let cut_arguments = vec!["-d", ",", "-f", START_END_RANGE_CUT, SAMPLE_FILE, "--complement"];
-    let sbcut_arguments = vec!["-d", ",","-f", START_END_RANGE_SBCUT, SAMPLE_FILE, "--complement"];
+    let cut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        START_END_RANGE_CUT,
+        SAMPLE_FILE,
+        "--complement",
+    ];
+    let sbcut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        START_END_RANGE_SBCUT,
+        SAMPLE_FILE,
+        "--complement",
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -339,7 +386,14 @@ fn test_04_3_complement_start_end_range() {
 fn test_04_4_complement_end_range() {
     // Arrange
     let cut_arguments = vec!["-d", ",", "-f", END_RANGE_CUT, SAMPLE_FILE, "--complement"];
-    let sbcut_arguments = vec!["-d", ",","-f", END_RANGE_SBCUT, SAMPLE_FILE, "--complement"];
+    let sbcut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        END_RANGE_SBCUT,
+        SAMPLE_FILE,
+        "--complement",
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -350,10 +404,25 @@ fn test_04_4_complement_end_range() {
 }
 
 #[test]
+#[ignore = "https://github.com/lyubolp/slightly-better-cut/issues/1"]
 fn test_04_5_complement_singe_start_range() {
     // Arrange
-    let cut_arguments = vec!["-d", ",", "-f", SINGLE_START_RANGE_CUT, SAMPLE_FILE, "--complement"];
-    let sbcut_arguments = vec!["-d", ",","-f", SINGLE_START_RANGE_SBCUT, SAMPLE_FILE, "--complement"];
+    let cut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        SINGLE_START_RANGE_CUT,
+        SAMPLE_FILE,
+        "--complement",
+    ];
+    let sbcut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        SINGLE_START_RANGE_SBCUT,
+        SAMPLE_FILE,
+        "--complement",
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -364,10 +433,25 @@ fn test_04_5_complement_singe_start_range() {
 }
 
 #[test]
+#[ignore = "https://github.com/lyubolp/slightly-better-cut/issues/1"]
 fn test_04_6_complement_single_single_start_range() {
     // Arrange
-    let cut_arguments = vec!["-d", ",", "-f", SINGLE_SINGLE_START_RANGE_CUT, SAMPLE_FILE, "--complement"];
-    let sbcut_arguments = vec!["-d", ",","-f", SINGLE_SINGLE_START_RANGE_SBCUT, SAMPLE_FILE, "--complement"];
+    let cut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        SINGLE_SINGLE_START_RANGE_CUT,
+        SAMPLE_FILE,
+        "--complement",
+    ];
+    let sbcut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        SINGLE_SINGLE_START_RANGE_SBCUT,
+        SAMPLE_FILE,
+        "--complement",
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -381,7 +465,7 @@ fn test_04_6_complement_single_single_start_range() {
 fn test_05_only_delimited() {
     // Arrange
     let cut_arguments = vec!["-d", ",", "-f", SINGLE_RANGE_CUT, SAMPLE_FILE, "-s"];
-    let sbcut_arguments = vec!["-d", ",","-f", SINGLE_RANGE_SBCUT, SAMPLE_FILE, "-s"];
+    let sbcut_arguments = vec!["-d", ",", "-f", SINGLE_RANGE_SBCUT, SAMPLE_FILE, "-s"];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -394,8 +478,24 @@ fn test_05_only_delimited() {
 #[test]
 fn test_06_1_output_delimiter_single_range() {
     // Arrange
-    let cut_arguments = vec!["-d", ",", "-f", SINGLE_RANGE_CUT, SAMPLE_FILE, "--output-delimiter", "-"];
-    let sbcut_arguments = vec!["-d", ",","-f", SINGLE_RANGE_SBCUT, SAMPLE_FILE, "--output_delimiter", "-"];
+    let cut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        SINGLE_RANGE_CUT,
+        SAMPLE_FILE,
+        "--output-delimiter",
+        "-",
+    ];
+    let sbcut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        SINGLE_RANGE_SBCUT,
+        SAMPLE_FILE,
+        "--output_delimiter",
+        "-",
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -408,8 +508,28 @@ fn test_06_1_output_delimiter_single_range() {
 #[test]
 fn test_06_2_output_delimiter_two_single_ranges() {
     // Arrange
-    let cut_arguments = vec!["-d", ",", "-f", SINGLE_RANGE_CUT, ",", SINGLE_RANGE_CUT_2, SAMPLE_FILE, "--output-delimiter", "-"];
-    let sbcut_arguments = vec!["-d", ",","-f", SINGLE_RANGE_SBCUT, ",", SINGLE_RANGE_SBCUT_2, SAMPLE_FILE, "--output_delimiter", "-"];
+    let cut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        SINGLE_RANGE_CUT,
+        ",",
+        SINGLE_RANGE_CUT_2,
+        SAMPLE_FILE,
+        "--output-delimiter",
+        "-",
+    ];
+    let sbcut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        SINGLE_RANGE_SBCUT,
+        ",",
+        SINGLE_RANGE_SBCUT_2,
+        SAMPLE_FILE,
+        "--output_delimiter",
+        "-",
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -422,8 +542,24 @@ fn test_06_2_output_delimiter_two_single_ranges() {
 #[test]
 fn test_06_3_output_delimiter_one_range() {
     // Arrange
-    let cut_arguments = vec!["-d", ",", "-f", START_END_RANGE_CUT, SAMPLE_FILE, "--output-delimiter", "-"];
-    let sbcut_arguments = vec!["-d", ",","-f", START_END_RANGE_SBCUT, SAMPLE_FILE, "--output_delimiter", "-"];
+    let cut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        START_END_RANGE_CUT,
+        SAMPLE_FILE,
+        "--output-delimiter",
+        "-",
+    ];
+    let sbcut_arguments = vec![
+        "-d",
+        ",",
+        "-f",
+        START_END_RANGE_SBCUT,
+        SAMPLE_FILE,
+        "--output_delimiter",
+        "-",
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -434,7 +570,7 @@ fn test_06_3_output_delimiter_one_range() {
 }
 
 #[test]
-fn test_07_1_nul_terminated_single_range(){
+fn test_07_1_nul_terminated_single_range() {
     // Arrange
     let cut_arguments = vec!["-c", SINGLE_RANGE_CUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
     let sbcut_arguments = vec!["-c", SINGLE_RANGE_SBCUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
@@ -448,7 +584,7 @@ fn test_07_1_nul_terminated_single_range(){
 }
 
 #[test]
-fn test_07_2_nul_terminated_start_range(){
+fn test_07_2_nul_terminated_start_range() {
     // Arrange
     let cut_arguments = vec!["-c", START_RANGE_CUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
     let sbcut_arguments = vec!["-c", START_RANGE_SBCUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
@@ -462,10 +598,15 @@ fn test_07_2_nul_terminated_start_range(){
 }
 
 #[test]
-fn test_07_3_nul_terminated_start_end_range(){
+fn test_07_3_nul_terminated_start_end_range() {
     // Arrange
     let cut_arguments = vec!["-c", START_END_RANGE_CUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
-    let sbcut_arguments = vec!["-c", START_END_RANGE_SBCUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+    let sbcut_arguments = vec![
+        "-c",
+        START_END_RANGE_SBCUT,
+        SAMPLE_FILE_NUL_TERMINATED,
+        "-z",
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -476,7 +617,7 @@ fn test_07_3_nul_terminated_start_end_range(){
 }
 
 #[test]
-fn test_07_4_nul_terminated_end_range(){
+fn test_07_4_nul_terminated_end_range() {
     // Arrange
     let cut_arguments = vec!["-c", END_RANGE_CUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
     let sbcut_arguments = vec!["-c", END_RANGE_SBCUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
@@ -490,10 +631,20 @@ fn test_07_4_nul_terminated_end_range(){
 }
 
 #[test]
-fn test_07_5_nul_terminated_single_start_range(){
+fn test_07_5_nul_terminated_single_start_range() {
     // Arrange
-    let cut_arguments = vec!["-c", SINGLE_START_RANGE_CUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
-    let sbcut_arguments = vec!["-c", SINGLE_START_RANGE_SBCUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+    let cut_arguments = vec![
+        "-c",
+        SINGLE_START_RANGE_CUT,
+        SAMPLE_FILE_NUL_TERMINATED,
+        "-z",
+    ];
+    let sbcut_arguments = vec![
+        "-c",
+        SINGLE_START_RANGE_SBCUT,
+        SAMPLE_FILE_NUL_TERMINATED,
+        "-z",
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -504,10 +655,20 @@ fn test_07_5_nul_terminated_single_start_range(){
 }
 
 #[test]
-fn test_07_6_nul_terminated_single_single_start_range(){
+fn test_07_6_nul_terminated_single_single_start_range() {
     // Arrange
-    let cut_arguments = vec!["-c", SINGLE_SINGLE_START_RANGE_CUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
-    let sbcut_arguments = vec!["-c", SINGLE_SINGLE_START_RANGE_SBCUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+    let cut_arguments = vec![
+        "-c",
+        SINGLE_SINGLE_START_RANGE_CUT,
+        SAMPLE_FILE_NUL_TERMINATED,
+        "-z",
+    ];
+    let sbcut_arguments = vec![
+        "-c",
+        SINGLE_SINGLE_START_RANGE_SBCUT,
+        SAMPLE_FILE_NUL_TERMINATED,
+        "-z",
+    ];
 
     // Act
     let cut_output = call_cut(cut_arguments);
@@ -535,7 +696,11 @@ fn call_sbcut(arguments: Vec<&str>) -> Result<(String, String, i32), String> {
 fn call_command(command: &str, arguments: Vec<&str>) -> Result<(String, String, i32), String> {
     let command_call = Command::new(command).args(arguments).output();
     match command_call {
-        Ok(output) =>Ok((String::from_utf8(output.stdout).unwrap(), String::from_utf8(output.stderr).unwrap(), output.status.code().unwrap())),
-        Err(_) => Err(String::from("Can't execute command"))
+        Ok(output) => Ok((
+            String::from_utf8(output.stdout).unwrap(),
+            String::from_utf8(output.stderr).unwrap(),
+            output.status.code().unwrap(),
+        )),
+        Err(_) => Err(String::from("Can't execute command")),
     }
 }
