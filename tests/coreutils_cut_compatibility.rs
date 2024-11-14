bae7c2e4707425680f3d7e2e76690bb1bff8de4b
Lyuboslav Karev <lyubo.karev@gmail.com>
2024-11-14 10:30:24 +0200

    Added more tests

--- tests/coreutils_cut_compatibility.rs
+++ tests/coreutils_cut_compatibility.rs
@@ -18,7 +18,9 @@ use std::process::Command;
 // STDIN
 
 static SINGLE_RANGE_CUT: &str = "2";
+static SINGLE_RANGE_CUT_2: &str = "4";
 static SINGLE_RANGE_SBCUT: &str = "1";
+static SINGLE_RANGE_SBCUT_2: &str = "3";
 
 static START_RANGE_CUT: &str = "2-";
 static START_RANGE_SBCUT: &str = "1:";
@@ -36,6 +38,7 @@ static SINGLE_SINGLE_START_RANGE_CUT: &str = "1,2,4-";
 static SINGLE_SINGLE_START_RANGE_SBCUT: &str = "0,1,3:";
 
 static SAMPLE_FILE: &str = "sample.csv";
+static SAMPLE_FILE_NUL_TERMINATED: &str = "sample_zero_terminated.txt";
 
 #[test]
 fn test_01_1_bytes_single_range(){
@@ -374,17 +377,163 @@ fn test_04_6_complement_single_single_start_range() {
     assert_eq!(cut_output, sbcut_output)
 }
 
+#[test]
+fn test_05_only_delimited() {
+    // Arrange
+    let cut_arguments = vec!["-d", ",", "-f", SINGLE_RANGE_CUT, SAMPLE_FILE, "-s"];
+    let sbcut_arguments = vec!["-d", ",","-f", SINGLE_RANGE_SBCUT, SAMPLE_FILE, "-s"];
+
+    // Act
+    let cut_output = call_cut(cut_arguments);
+    let sbcut_output = call_sbcut(sbcut_arguments);
+
+    // Assert
+    assert_eq!(cut_output, sbcut_output)
+}
+
+#[test]
+fn test_06_1_output_delimiter_single_range() {
+    // Arrange
+    let cut_arguments = vec!["-d", ",", "-f", SINGLE_RANGE_CUT, SAMPLE_FILE, "--output-delimiter", "-"];
+    let sbcut_arguments = vec!["-d", ",","-f", SINGLE_RANGE_SBCUT, SAMPLE_FILE, "--output_delimiter", "-"];
+
+    // Act
+    let cut_output = call_cut(cut_arguments);
+    let sbcut_output = call_sbcut(sbcut_arguments);
+
+    // Assert
+    assert_eq!(cut_output, sbcut_output)
+}
+
+#[test]
+fn test_06_2_output_delimiter_two_single_ranges() {
+    // Arrange
+    let cut_arguments = vec!["-d", ",", "-f", SINGLE_RANGE_CUT, ",", SINGLE_RANGE_CUT_2, SAMPLE_FILE, "--output-delimiter", "-"];
+    let sbcut_arguments = vec!["-d", ",","-f", SINGLE_RANGE_SBCUT, ",", SINGLE_RANGE_SBCUT_2, SAMPLE_FILE, "--output_delimiter", "-"];
+
+    // Act
+    let cut_output = call_cut(cut_arguments);
+    let sbcut_output = call_sbcut(sbcut_arguments);
+
+    // Assert
+    assert_eq!(cut_output, sbcut_output)
+}
+
+#[test]
+fn test_06_3_output_delimiter_one_range() {
+    // Arrange
+    let cut_arguments = vec!["-d", ",", "-f", START_END_RANGE_CUT, SAMPLE_FILE, "--output-delimiter", "-"];
+    let sbcut_arguments = vec!["-d", ",","-f", START_END_RANGE_SBCUT, SAMPLE_FILE, "--output_delimiter", "-"];
+
+    // Act
+    let cut_output = call_cut(cut_arguments);
+    let sbcut_output = call_sbcut(sbcut_arguments);
+
+    // Assert
+    assert_eq!(cut_output, sbcut_output)
+}
+
+#[test]
+fn test_07_1_nul_terminated_single_range(){
+    // Arrange
+    let cut_arguments = vec!["-c", SINGLE_RANGE_CUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+    let sbcut_arguments = vec!["-c", SINGLE_RANGE_SBCUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+
+    // Act
+    let cut_output = call_cut(cut_arguments);
+    let sbcut_output = call_sbcut(sbcut_arguments);
+
+    // Assert
+    assert_eq!(cut_output, sbcut_output)
+}
+
+#[test]
+fn test_07_2_nul_terminated_start_range(){
+    // Arrange
+    let cut_arguments = vec!["-c", START_RANGE_CUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+    let sbcut_arguments = vec!["-c", START_RANGE_SBCUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+
+    // Act
+    let cut_output = call_cut(cut_arguments);
+    let sbcut_output = call_sbcut(sbcut_arguments);
+
+    // Assert
+    assert_eq!(cut_output, sbcut_output)
+}
+
+#[test]
+fn test_07_3_nul_terminated_start_end_range(){
+    // Arrange
+    let cut_arguments = vec!["-c", START_END_RANGE_CUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+    let sbcut_arguments = vec!["-c", START_END_RANGE_SBCUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+
+    // Act
+    let cut_output = call_cut(cut_arguments);
+    let sbcut_output = call_sbcut(sbcut_arguments);
+
+    // Assert
+    assert_eq!(cut_output, sbcut_output)
+}
+
+#[test]
+fn test_07_4_nul_terminated_end_range(){
+    // Arrange
+    let cut_arguments = vec!["-c", END_RANGE_CUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+    let sbcut_arguments = vec!["-c", END_RANGE_SBCUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+
+    // Act
+    let cut_output = call_cut(cut_arguments);
+    let sbcut_output = call_sbcut(sbcut_arguments);
+
+    // Assert
+    assert_eq!(cut_output, sbcut_output)
+}
+
+#[test]
+fn test_07_5_nul_terminated_single_start_range(){
+    // Arrange
+    let cut_arguments = vec!["-c", SINGLE_START_RANGE_CUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+    let sbcut_arguments = vec!["-c", SINGLE_START_RANGE_SBCUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+
+    // Act
+    let cut_output = call_cut(cut_arguments);
+    let sbcut_output = call_sbcut(sbcut_arguments);
+
+    // Assert
+    assert_eq!(cut_output, sbcut_output)
+}
+
+#[test]
+fn test_07_6_nul_terminated_single_single_start_range(){
+    // Arrange
+    let cut_arguments = vec!["-c", SINGLE_SINGLE_START_RANGE_CUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+    let sbcut_arguments = vec!["-c", SINGLE_SINGLE_START_RANGE_SBCUT, SAMPLE_FILE_NUL_TERMINATED, "-z"];
+
+    // Act
+    let cut_output = call_cut(cut_arguments);
+    let sbcut_output = call_sbcut(sbcut_arguments);
+
+    // Assert
+    assert_eq!(cut_output, sbcut_output)
+}
+
 fn call_cut(arguments: Vec<&str>) -> Result<(String, String, i32), String> {
+    println!("cut: {}", arguments.join(" "));
     call_command("cut", arguments)
 }
 
 fn call_sbcut(arguments: Vec<&str>) -> Result<(String, String, i32), String> {
-    call_command("./target/debug/sbcut", arguments)
+    println!("sbcut: {}", arguments.join(" "));
+
+    // TODO - Add comment about this
+    let mut extended_arguments: Vec<&str> = vec!["--always_show_no_delimited_lines"];
+    extended_arguments.extend(arguments);
+
+    call_command("./target/debug/sbcut", extended_arguments)
 }
 
 fn call_command(command: &str, arguments: Vec<&str>) -> Result<(String, String, i32), String> {
     let command_call = Command::new(command).args(arguments).output();
-
     match command_call {
         Ok(output) =>Ok((String::from_utf8(output.stdout).unwrap(), String::from_utf8(output.stderr).unwrap(), output.status.code().unwrap())),
         Err(_) => Err(String::from("Can't execute command"))
