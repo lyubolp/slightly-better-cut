27864bd4928490b15236219d789eeb96fce24035
Lyuboslav Karev <lyubo.karev@gmail.com>
2024-11-26 10:11:11 +0200

    Added a fix for negative numbers in the CLI

--- src/cli.rs
+++ src/cli.rs
@@ -5,14 +5,14 @@ pub fn build_cli() -> Command {
     Command::new("sbcut")
         .version("0.1")
         .about("Slightly better cut")
-        .arg(arg!(-b --bytes <LIST>))
-        .arg(arg!(-c --characters <LIST>))
+        .arg(arg!(-b --bytes <LIST>).allow_negative_numbers(true))
+        .arg(arg!(-c --characters <LIST>).allow_negative_numbers(true))
         .arg(
             arg!(-d --delimiter <DELIM>)
                 .requires("fields")
                 .default_value("\t"),
         )
-        .arg(arg!(-f --fields <LIST>))
+        .arg(arg!(-f --fields <LIST>).allow_negative_numbers(true))
         .arg(arg!(--complement).action(ArgAction::SetTrue))
         .arg(arg!(-s --only_delimited).action(ArgAction::SetTrue))
         .arg(arg!(--always_show_no_delimited_lines).action(ArgAction::SetTrue)) // If a line is not delimited, always show it in full (cut compatibility)
